8b0222e4a6eddeb6da0825c0f49035a9b96a9143
Ruchika Modi <ruchika.modi.apm13@itbhu.ac.in>
2017-07-22 17:44:20 +0530

    Create Valid Anagram.cpp

---
+++ Valid Anagram.cpp
@@ -0,0 +1,26 @@
+/*
+
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+     sort(s.begin(),s.end())   ;
+        sort(t.begin(),t.end());
+        if(s==t)
+            return true;
+        return false;
+    }
+};
